48039c45fbe6ad2f2d79a4cda8cbb968460eb12f
NotaRobotexe <mt2.sindicate.peto@gmail.com>
2018-02-22 18:40:07 +0100

    object drawing

--- PyScript/Script.py
+++ PyScript/Script.py
@@ -1,8 +1,19 @@
 import Arm as arm
+import numpy as np
+import cv2
 
-arm.InitCom("127.0.0.1")
-mess = arm.ReadInput()
-print(mess)
-if mess="ACK":
-    arm.SetPosition(90,120,40,55,-1,-1)
+arm.InitCom()
+cap = cv2.VideoCapture("C:\\Users\\mt2si\\Desktop\\projekty\\S.O.C Robotic Arm\\Robotic Arm\\PyScript\\test.mp4")
+while(1):
+	_, frame = cap.read()
+	BGR = np.uint8([[[51,91,222]]])
+	_,a = arm.BasicColorRecognition(frame,BGR,20,7000)
+	if a == 0:
+		break
+	arm.DrawTargetsOnVideo(a)
+	arm.SendMessage("benis")
+	print(a)
+	k = cv2.waitKey(30) & 0xFF
+	if k == 27:
+		break
 arm.CloseCom()
